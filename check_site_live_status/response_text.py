7cf904ea6e3b9170d063557f68dfd29e7b4ad53a
Uniyal <abhishekuniyal09@gmail.com>
2018-06-14 22:46:12 +0530

    check if website is up or not

---
+++ check_site_live_status/response_text.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = input('Enter the name of site to check (format google.com): ')
+res = requests.get('https://isitup.org/' + url)
+soup = BeautifulSoup(res.text, 'lxml')
+
+container = soup.find('div', {'id': 'container'})
+status = container.find('p')
+print(status.text)
